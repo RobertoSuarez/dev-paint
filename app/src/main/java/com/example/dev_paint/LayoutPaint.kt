f7d85c763d2877f96b1a652ab975a6d85fda3b27
RobertoSuarez <electrosonix12@gmail.com>
2023-03-19 17:33:31 -0500

    desarrollo de tareas

--- app/src/main/java/com/example/dev_paint/LayoutPaint.kt
+++ app/src/main/java/com/example/dev_paint/LayoutPaint.kt
@@ -3,8 +3,11 @@ package com.example.dev_paint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
+import com.example.dev_paint.models.User
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomnavigation.LabelVisibilityMode
 
@@ -12,6 +15,7 @@ class LayoutPaint : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var frameLayout: FrameLayout
+    private var rol: String = "Docente"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,28 @@ class LayoutPaint : AppCompatActivity() {
 
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         frameLayout = findViewById(R.id.frameLayout)
+
+        val menu: Menu = bottomNavigationView.menu
+        val menuItemAsignar: MenuItem = menu.findItem(R.id.item_asignar)
+
+        menuItemAsignar.isVisible = false
+
+        User().getCurrentUser()
+            .addOnSuccessListener { document ->
+                val usuario = document.toObject(User::class.java)
+                this.rol = usuario?.rol.toString()
+
+                if (usuario?.rol == "Docente") {
+                    menuItemAsignar.isVisible = true
+                    menuItemAsignar.setIcon(R.drawable.baseline_add_task_24)
+                }
+
+                if (usuario?.rol == "Estudiante") {
+                    menuItemAsignar.isVisible = true
+                    menuItemAsignar.title = "Tareas"
+                    menuItemAsignar.setIcon(R.drawable.baseline_task_24)
+                }
+            }
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when(item.itemId) {
@@ -34,6 +60,16 @@ class LayoutPaint : AppCompatActivity() {
                 // establecemos el fragment Calificaciones
                 R.id.item_puntaje -> {
                     switchFragment(CalificacionesFragment())
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.item_asignar -> {
+                    if (rol == "Docente") {
+                        switchFragment(AsignarTareaFragment())
+                    } else {
+                        switchFragment(TareasFragment())
+                    }
+
                     return@setOnItemSelectedListener true
                 }
 
